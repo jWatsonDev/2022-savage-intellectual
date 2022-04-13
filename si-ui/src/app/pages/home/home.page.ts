46a8e36b167b683bb80d04dad00d3b4675293dd6
Jay Watson <jaywatson1982@gmail.com>
2022-04-13 16:54:20 -0500

    Add discipline display to main page

--- si-ui/src/app/pages/home/home.page.ts
+++ si-ui/src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CATEGORY_LIST } from 'src/app/helpers/config/category';
+import { Discipline } from 'src/app/models/discipline';
+import { Status } from 'src/app/models/status';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  disciplines: Array<Discipline>;
+  statuses: Array<Status>;
+  statusDisciplines: Array<Status>;
 
   ngOnInit() {
+    this.getDisciplinesAndStatuses();
+  }
+
+  getDisciplinesAndStatuses() {
+    this.buildStatusDisciplines();
+    // TODO: 
+    // const today = new Date().toISOString().slice(0, 10);
+    // forkJoin(
+    //   this._dataService.getDisciplinesByUsername(),
+    //   this._dataService.getStatusesByUsernameAndDate(today)
+    // ).subscribe(res => {
+    //   this.disciplines = res[0];
+    //   this.statuses = res[1];
+    //   // console.log(res);
+    //   this.buildStatusDisciplines();
+    // });
+  }
+
+  buildStatusDisciplines() {
+    this.statusDisciplines = [
+      {
+        disciplineId: 1,
+        name: 'Read Bible',
+        complete: true,
+        category: 'personal'
+      },
+      {
+        disciplineId: 2,
+        name: '15m Professional Dev',
+        complete: true,
+        category: 'professional'
+      },
+      {
+        disciplineId: 3,
+        name: '~Gallon of Water',
+        complete: true,
+        category: 'physical'
+      },
+      {
+        disciplineId: 4,
+        name: 'Workout',
+        complete: false,
+        category: 'physical'
+      }
+    ];
+    // TODO:
+    // this.disciplines.forEach(discipline => {
+    //   let status: Status;
+    //   status = this.statuses.find(s => s.disciplineId === discipline.id);
+    //   // console.log('status', status, discipline.id)
+    //   if (status) {
+    //     status.name = discipline.name;
+    //     status.category = discipline.category;
+    //   } else {
+    //     status = {
+    //       name: discipline.name,
+    //       disciplineId: discipline.id,
+    //       complete: false,
+    //       date: new Date(),
+    //       username: discipline.username,
+    //       category: discipline.category
+    //     };
+    //   }
+    //   this.statusDisciplines.push(status);
+    // });
+
+    // console.log(this.statusDisciplines)
+  }
+
+  // TODO: update discipline status
+  valueChanged(discipline) {
+    // console.log(discipline);
+    // this._dataService.saveStatus(discipline).subscribe(res => {
+    //   console.log(res);
+    // });
   }
 
 }
