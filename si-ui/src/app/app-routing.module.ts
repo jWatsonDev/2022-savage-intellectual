18faaf19041d538734f237cdcdfb0c2d3b4ffbf2
Jay Watson <jaywatson1982@gmail.com>
2022-04-13 16:53:59 -0500

    Remove sub routes from main routing module

--- si-ui/src/app/app-routing.module.ts
+++ si-ui/src/app/app-routing.module.ts
@@ -5,18 +5,6 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'create-edit-discipline',
-    loadChildren: () => import('./pages/create-edit-discipline/create-edit-discipline.module').then( m => m.CreateEditDisciplinePageModule)
   }
 ];
 @NgModule({
